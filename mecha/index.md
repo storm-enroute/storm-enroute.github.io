7161e99d23a9f1992a93e4aa6fb82b43e011c7e4
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2015-05-23 22:19:09 +0200

    Add mecha page.

---
+++ mecha/index.md
@@ -0,0 +1,19 @@
+---
+layout: projmainpage
+projectname: Mecha
+projectpath: mecha
+logoname: mecha-logo-64.png
+title: Mecha
+permalink: /mecha/index.html
+logostyle: "color: #5f5f5f;"
+---
+
+<span id="readmebox"></span>
+
+<script src="/resources/js/setlicense.js"></script>
+<script>
+  setContent(
+    "readmebox",
+    "https://api.github.com/repos/storm-enroute/mecha/contents/README.md",
+    "mecha-logo-64.png")
+</script>
--- resources/js/setlicense.js
+++ resources/js/setlicense.js
@@ -1,14 +1,17 @@
 
-
-function setLicense(url, logoname) {
-  var licensebox = document.getElementById("licensebox");
+function setContent(contentboxname, url, logoname) {
+  var contentbox = document.getElementById(contentboxname);
   $.get(url, function(data) {
     var icon = document.createElement('IMG');
     icon.setAttribute("src", "/resources/images/" + logoname);
     icon.setAttribute("height", 48);
     var ltxt = window.atob(data.content);
     ltxt = ltxt.replace(/\t/g, '    ').replace(/  /g, '&nbsp; ').replace(/  /g, ' &nbsp;').replace(/\r\n|\n|\r/g, '<br />');
-    licensebox.appendChild(icon);
-    licensebox.innerHTML = licensebox.innerHTML + '<br/>' + ltxt;
+    contentbox.appendChild(icon);
+    contentbox.innerHTML = contentbox.innerHTML + '<br/>' + ltxt;
   });
+}
+
+function setLicense(url, logoname) {
+  setContent(url, logoname, "licensebox")
 }
